ea27ad8aaafb6c8ea5c17a442e2d58b867e84dfd
Matvey Kudryavtsev <arzontum@gmail.com>
2025-11-22 22:58:43 +0500

    started to doing books page

--- frontend/sabantui/src/app/app.routes.ts
+++ frontend/sabantui/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { BooksComponent} from './components/books/books.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    // { path: 'books', component: BooksComponent },
+    { path: 'books', component: BooksComponent },
     // { path: 'events', component: EventsComponent },
     // { path: 'coworking', component: CoworkingComponent },
     { path: 'profile', component: ProfileComponent },
