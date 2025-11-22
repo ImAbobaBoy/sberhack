49a1433df9ddf5d3bb9a05660e372b4d8054cdbd
StepanMkr <152437585+StepanMkr@users.noreply.github.com>
2025-11-22 12:46:59 +0500

    install taiga

--- frontend/sabantui/src/app/app.component.ts
+++ frontend/sabantui/src/app/app.component.ts
@@ -1,12 +1,16 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER, TuiButtonModule } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule, TuiButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+    providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
 })
 export class AppComponent {
   title = 'sabantui';
+  a = 'tuiIconCheck'
 }
