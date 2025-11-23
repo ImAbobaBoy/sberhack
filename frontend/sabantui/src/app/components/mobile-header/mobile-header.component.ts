28d93445bfd2e5d49fa922da9ef9ccebd3856a44
Matvey Kudryavtsev <arzontum@gmail.com>
2025-11-23 07:28:42 +0500

    changed events and header styles

--- frontend/sabantui/src/app/components/mobile-header/mobile-header.component.ts
+++ frontend/sabantui/src/app/components/mobile-header/mobile-header.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {TuiSvgModule} from '@taiga-ui/core';
+import { TuiSvgModule } from '@taiga-ui/core';
+import { RouterModule } from '@angular/router';  // Импортируйте RouterModule
 
 @Component({
   selector: 'app-mobile-header',
   templateUrl: './mobile-header.component.html',
-  imports: [CommonModule, TuiSvgModule],
+  imports: [CommonModule, TuiSvgModule, RouterModule],  // Добавьте RouterModule сюда
   standalone: true,
   styleUrls: ['./mobile-header.component.css']
 })
