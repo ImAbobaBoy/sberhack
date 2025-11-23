2005a6f08720b13e206b35461c63975c0fb1f61a
Matvey Kudryavtsev <arzontum@gmail.com>
2025-11-23 06:45:01 +0500

    changed routes

--- frontend/sabantui/src/app/components/mobile-header/mobile-header.component.ts
+++ frontend/sabantui/src/app/components/mobile-header/mobile-header.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {TuiSvgModule} from '@taiga-ui/core';
 
 @Component({
   selector: 'app-mobile-header',
   templateUrl: './mobile-header.component.html',
-  imports: [CommonModule],
+  imports: [CommonModule, TuiSvgModule],
   standalone: true,
   styleUrls: ['./mobile-header.component.css']
 })
 export class MobileHeaderComponent {
   menuOpen = false;
+  logoPath: string = 'assets/Vector.svg';
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
