98af8b43125a97b9c7893797a26e287801bd6f20
Matvey Kudryavtsev <arzontum@gmail.com>
2025-11-23 09:59:50 +0500

    added create event component 2

--- frontend/sabantui/src/app/app.routes.ts
+++ frontend/sabantui/src/app/app.routes.ts
@@ -3,11 +3,24 @@ import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { EmailRegStepComponent } from './components/email-reg-step/email-reg-step.component';
 import { CodeVerifyStepComponent } from './components/code-verify-step/code-verify-step.component';
+import {BooksComponent} from './components/books/books.component';
+import {EventsComponent} from './components/events/events.component';
+import {TtrComponent} from './components/ttr/ttr.component';
+import {MpPageComponent} from './components/mp-page/mp-page.component';
+import {BookPageComponent} from './components/book-page/book-page.component';
+import {CreateEventComponent} from './components/create-event/create-event.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    // { path: 'books', component: BooksComponent },
-    // { path: 'events', component: EventsComponent },
+    { path: 'books', component: BooksComponent },
+    { path: 'book', component: ProfileComponent },
+    { path: 'book/:id', component: BookPageComponent },
+    { path: 'ttr', component: TtrComponent },
+
+
+    { path: 'events', component: EventsComponent },
+    { path: 'event/:id', component: MpPageComponent },
+    { path: 'create-event', component: CreateEventComponent },
     // { path: 'coworking', component: CoworkingComponent },
     { path: 'profile', component: ProfileComponent },
     { path: 'email-step', component: EmailRegStepComponent },
